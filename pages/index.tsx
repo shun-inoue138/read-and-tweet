d34f3c39868b1ab36314ff5c65670ffbf70b8ebc
inoue <ht8sk13@icloud.com>
2023-01-17 20:08:59 +0900

    Taskitemコンポーネントの途中。propsの値によって異なるjsxを返すようにした

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import TaskItem from "src/components/TaskItem";
+import { completedTask, incompletedTask } from "src/utils/dummyData/tasks";
 
 const index = () => {
-  return <div className="bg-red-200">index</div>;
+  return (
+    <div>
+      <TaskItem {...incompletedTask} />
+      <TaskItem {...completedTask} />
+    </div>
+  );
 };
 
 export default index;
