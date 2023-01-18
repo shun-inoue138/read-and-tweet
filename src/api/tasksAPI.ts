b80b6569fafb979d64dd4dac3cbbade2e479ef93
inoue <ht8sk13@icloud.com>
2023-01-18 17:41:52 +0900

    タスクの編集と特定のidのタスクをfetchする処理（mock版）の作成

--- src/api/tasksAPI.ts
+++ src/api/tasksAPI.ts
@@ -10,8 +10,20 @@ export const useGetAllTasks = () => {
   const { data, error, mutate, isLoading } = useSWR<Task[]>(url, fetcher);
   return { tasks: data, error, mutate, isLoading };
 };
+export const useGetTask = (id: number) => {
+  const url = `/tasks/${id}`;
+  const fetcher: Fetcher<Task, string> = () => {
+    return axiosClient.get(url).then((res) => res.data);
+  };
+  const { data, error, mutate, isLoading } = useSWR<Task>(url, fetcher);
+  return { task: data, error, mutate, isLoading };
+};
 
 export const deleteTask = (id: number) => {
   const url = `/tasks/${id}`;
   return axiosClient.delete(url);
+};
+export const editTask = (id: number, task: Task) => {
+  const url = `/tasks/${id}`;
+  return axiosClient.put(url, task);
 };
