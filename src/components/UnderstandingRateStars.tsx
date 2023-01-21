925568adf6c42f6465269c937ad2cdfaa696d6a5
inoue <ht8sk13@icloud.com>
2023-01-21 11:04:05 +0900

    understandingRateステートの宣言場所をひとつ上の階層にした

--- src/components/UnderstandingRateStars.tsx
+++ src/components/UnderstandingRateStars.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import UnderstandingRateStar from "./UnderstandingRateStar";
 
-const UnderstandingRateStars = ({ maxnumber = 5 }) => {
-  const [understandingRate, setUnderstandingRate] = React.useState(1);
+const UnderstandingRateStars = ({
+  maxnumber = 5,
+  understandingRate,
+  setUnderstandingRate,
+}) => {
   return (
     <ul className="flex gap-1">
       {Array.from({ length: maxnumber }, (_, i) => i + 1).map((number) => {
