918a15985ed078d2ac3fcc0c6f97687a7582487b
inoue <ht8sk13@icloud.com>
2023-01-31 23:57:04 +0900

    非ログイン状態で各ページに飛ぼうとすると強制的にログインページへ遷移させる

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,10 @@ import "@/styles/global.css";
 import type { AppProps } from "next/app";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useCheckIsLogin } from "src/hooks/useCheckIsLogin";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  useCheckIsLogin();
   return (
     <>
       <Component {...pageProps} />
