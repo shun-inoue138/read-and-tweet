8802818d8b96bdf771291e13cebff3c76a380362
inoue <ht8sk13@icloud.com>
2023-01-31 23:55:39 +0900

    signinページとsignupページのURLを定数化

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,3 +1,5 @@
 export const Default_Input_Due_Days = 7;
 export const Default_Input_Understanding_Rate = 1;
 export const Default_Max_Number_of_Understanding_Rate = 5;
+export const Signin_URL = "/signin";
+export const Signup_URL = "/signup";
