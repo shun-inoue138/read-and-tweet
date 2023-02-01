72db905f84760c8fd554d1151f2fcd2f6f533c40
inoue <ht8sk13@icloud.com>
2023-02-01 13:05:52 +0900

    コメント追加のみ

--- src/utils/types/Task.ts
+++ src/utils/types/Task.ts
@@ -3,6 +3,7 @@ export type Task = {
   _id: string;
   title: string;
   randomNote: string;
+  //categoriesはcategoriesコレクションから取得
   categories: string[];
   postContent: string;
   understandingRate: 1 | 2 | 3 | 4 | 5;
